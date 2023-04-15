f7deae41bdce440bfd3e5e8640a831a4e0b8fa0e
RedMuri <celovekda@gmail.com>
2023-04-15 22:33:14 +0300

    Add layout for creating card

--- app/src/main/java/com/example/cardfate/presentation/fragment/CreateCardFragment.kt
+++ app/src/main/java/com/example/cardfate/presentation/fragment/CreateCardFragment.kt
@@ -1,34 +1,29 @@
 package com.example.cardfate.presentation.fragment
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import androidx.fragment.app.Fragment
 import com.example.cardfate.R
 
 
-class CardFragment : Fragment() {
-
+class CreateCardFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        return inflater.inflate(R.layout.fragment_card, container, false)
+        return inflater.inflate(R.layout.fragment_create_card, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        view.findViewById<ImageView>(R.id.cardView).apply {
+        view.findViewById<ImageView>(R.id.iv_user_image).apply {
             clipToOutline = true
         }
     }
 
-    companion object {
 
-        fun newInstance() =
-            CardFragment()
-    }
 }
