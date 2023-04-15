06a0bab1051497e03e930805e7ff5dfe4f1837e8
RedMuri <celovekda@gmail.com>
2023-04-15 16:40:32 +0300

    Add user entity

--- app/src/main/java/com/example/cardfate/domain/entity/User.kt
+++ app/src/main/java/com/example/cardfate/domain/entity/User.kt
@@ -1,19 +1,15 @@
-package com.example.homehelper.domain.entities
+package com.example.cardfate.domain.entity
 
 data class User(
-    var id: String = "none",
-    var surname: String = "none",
-    var name: String = "none",
-    var patronymic: String = "none",
-    var gender: String = "none",
-    var age: Int = 0,
-    var passport: String = "none",
-    var email: String = "none",
-    var city: String = "none",
-    var street: String = "none",
-    var house: Int = 0,
-    var flat: Int = 0,
-    var login: String = "none",
-    var password: String = "none"
+    val id: Int? = null,
+    val photoUrl: String? = null,
+    val login: String,
+    val password: String,
+    val fullName: String? = null,
+    val bio: String? = null,
+    val qrUrl: String? = null,
+    val idLink: Int? = null,
+    val idCard: Int? = null,
+    val idSkill: Int? = null,
 )
 
