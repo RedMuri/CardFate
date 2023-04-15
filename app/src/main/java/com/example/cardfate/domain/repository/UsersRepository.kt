adc84a6dbeca6d0ee9ac06bff33c775fc921fe6c
RedMuri <celovekda@gmail.com>
2023-04-15 18:26:29 +0300

    Add auth methods with Firebase

--- app/src/main/java/com/example/cardfate/domain/repository/UsersRepository.kt
+++ app/src/main/java/com/example/cardfate/domain/repository/UsersRepository.kt
@@ -1,10 +1,8 @@
 package com.example.cardfate.domain.repository
 
-import com.example.cardfate.domain.entity.User
-
 interface UsersRepository {
 
-    suspend fun signIn(user: User, callback: () -> Unit)
+    suspend fun signIn(login: String, password: String, callback: (String) -> Unit)
 
-    suspend fun logIn(phone: String, password: String, callback: () -> Unit)
+    suspend fun logIn(login: String, password: String, callback: (String) -> Unit)
 }
